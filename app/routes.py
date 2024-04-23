136681e5e8a08782ab744e5988270a15dc46de5b
Divrr <adibrohan54@gmail.com>
2024-04-24 07:50:33 +0800

    Added jinja templating to sidebar

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,21 @@
 from flask import render_template
 from app import app
 
+useroffers = [
+    {'title': 'I can help debug Python files.'},
+    {'title': 'I can peer-review academic writing.'},
+    {'title': 'Down to give advice on your resume!'}
+]
+
+userrequests = [
+    {'title': 'I need help fixing my doornob.'},
+    {'title': 'I need help with my math homework.'},
+    {'title': 'Teach me to bake a cake.'},
+]
+
+
+
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("mainpage.html")
+    return render_template("mainpage.html", useroffers=useroffers, userrequests=userrequests)
