633bf256df8295234aa9e0e7a733f6d4477e15b1
Divrr <96827300+Divrr@users.noreply.github.com>
2024-04-24 15:30:33 +0800

    reorganising files and links

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,16 @@
 from flask import render_template
+from flask import Flask, request, jsonify
+
 from app import app
 
 @app.route("/")
 @app.route("/index")
 def index():
     return render_template("mainpage.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
 
 auth_table = []
 
