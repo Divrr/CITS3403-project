03003067c93731e41e72211fa9f2b1e362494afb
Divrr <96827300+Divrr@users.noreply.github.com>
2024-04-25 17:35:09 +0800

    re-designed as a user-activity schema

--- app/models.py
+++ app/models.py
@@ -1,82 +1,32 @@
 from datetime import datetime, timezone
+from sqlalchemy import String, Integer, ForeignKey, Enum
+from sqlalchemy.orm import Mapped, WriteOnlyMapped, mapped_column, relationship
 from typing import Optional
-import sqlalchemy as sa
-import sqlalchemy.orm as so
 from app import db
 
 class User(db.Model):
-    # Users Table schema:
-    # id (Primary Key)
-    # username
-    # email
-    # password_hash
-    # created_at
-    # updated_at
+    _id          : Mapped[int]           = mapped_column(primary_key=True)
+    username     : Mapped[str]           = mapped_column(String(64), index=True, unique=True)
+    email        : Mapped[str]           = mapped_column(String(128), index=True, unique=True)
+    password_hash: Mapped[Optional[str]] = mapped_column(String(256))
 
-    id           : so.Mapped[int]           = so.mapped_column(primary_key=True)
-
-    username     : so.Mapped[str]           = so.mapped_column(sa.String(64), index=True, unique=True)
-    email        : so.Mapped[str]           = so.mapped_column(sa.String(120), index=True, unique=True)
-    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
-
-    created_at   : so.Mapped[datetime]      = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
-    updated_at   : so.Mapped[datetime]      = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
-
-    requests     : so.WriteOnlyMapped['Request'] = so.relationship(back_populates='user')
-    offers       : so.WriteOnlyMapped['Offer']   = so.relationship(back_populates='user')
+    authored     : WriteOnlyMapped['Activity'] = relationship(back_populates='author', foreign_keys='Activity.author_id')
+    accepted     : WriteOnlyMapped['Activity'] = relationship(back_populates='acceptor', foreign_keys='Activity.acceptor_id')
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
 
-class Request(db.Model):
-    # Requests Table schema:
-    # id (Primary Key)
-    # user_id (Foreign Key from Users)
-    # title
-    # description
-    # status
-    # created_at (indexed)
-    # updated_at
+class Activity(db.Model):
+    _id          : Mapped[int]      = mapped_column(primary_key=True)
+    author_id    : Mapped[int]      = mapped_column(ForeignKey(User._id))
+    acceptor_id  : Mapped[int]      = mapped_column(ForeignKey(User._id))
+    kind         : Mapped[str]      = mapped_column(Enum('Request', 'Offer'), nullable=False)
+    status       : Mapped[str]      = mapped_column(Enum('Open', 'Closed', 'Pending', 'Completed'), nullable=False, default='Open')
+    description  : Mapped[str]      = mapped_column(String(256))
+    updated_at   : Mapped[datetime] = mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
 
-    id           : so.Mapped[int]           = so.mapped_column(primary_key=True)
-    user_id      : so.Mapped[int]           = so.mapped_column(sa.ForeignKey(User.id), index=True)
-
-    title        : so.Mapped[str]           = so.mapped_column(sa.String(64))
-    description  : so.Mapped[str]           = so.mapped_column(sa.String(256))
-    status       : so.Mapped[str]           = sa.Column(sa.Enum('Open', 'Closed', 'Pending', 'Completed'), nullable=False, default='Open')
-
-    created_at   : so.Mapped[datetime]      = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
-    updated_at   : so.Mapped[datetime]      = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
-
-
-    user         : so.Mapped[User]             = so.relationship(back_populates='requests')
-    offers       : so.WriteOnlyMapped['Offer'] = so.relationship(back_populates='request')
-
-
-    def __repr__(self):
-        return '<Request {}: "{}">'.format(self.title, self.description)
-    
-class Offer(db.Model):
-    # Offers Table:
-    # id (Primary Key)
-    # request_id (Foreign Key from Requests)
-    # user_id (Foreign Key from Users)
-    # details
-    # created_at (indexed)
-    # updated_at
-
-    id           : so.Mapped[int]           = so.mapped_column(primary_key=True)
-    request_id   : so.Mapped[int]           = so.mapped_column(sa.ForeignKey(Request.id), index=True)
-    user_id      : so.Mapped[int]           = so.mapped_column(sa.ForeignKey(User.id), index=True)
-
-    description  : so.Mapped[str]           = so.mapped_column(sa.String(256))
-    status       : so.Mapped[str]           = so.mapped_column(sa.Enum('Pending', 'Accepted', 'Rejected', 'Withdrawn'), nullable=False, default='Pending')
-
-    created_at   : so.Mapped[datetime]      = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
-    updated_at   : so.Mapped[datetime]      = so.mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
-
-    user         : so.Mapped[User]    = so.relationship(back_populates='offers')
-    request      : so.Mapped[Request] = so.relationship(back_populates='offers')
+    author       : Mapped[User] = relationship(back_populates='authored', foreign_keys=[author_id])
+    acceptor     : Mapped[User] = relationship(back_populates='accepted', foreign_keys=[acceptor_id])
 
 
     def __repr__(self):
