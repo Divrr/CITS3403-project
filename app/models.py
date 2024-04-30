63ffcca93fcafb823721a65f73b4678b8438583e
Divrr <96827300+Divrr@users.noreply.github.com>
2024-04-30 10:59:00 +0800

    updated user-activity model to align with sqlalchemy and our designs
    in particular, changed  to uid=501(deepdivrr) gid=20(staff) groups=20(staff),12(everyone),61(localaccounts),79(_appserverusr),80(admin),81(_appserveradm),98(_lpadmin),701(com.apple.sharepoint.group.1),33(_appstore),100(_lpoperator),204(_developer),250(_analyticsusers),395(com.apple.access_ftp),398(com.apple.access_screensharing),399(com.apple.access_ssh),400(com.apple.access_remote_ae), changed  to , added  requirements to everything, added 'category' field, and edited max char length for category and description.

--- app/models.py
+++ app/models.py
@@ -5,9 +5,9 @@ from typing import Optional
 from app import db
 
 class User(db.Model):
-    _id          : Mapped[int]           = mapped_column(primary_key=True)
-    username     : Mapped[str]           = mapped_column(String(64), index=True, unique=True)
-    email        : Mapped[str]           = mapped_column(String(128), index=True, unique=True)
+    id           : Mapped[int]           = mapped_column(primary_key=True)
+    username     : Mapped[str]           = mapped_column(String(64), nullable=False, index=True, unique=True)
+    email        : Mapped[str]           = mapped_column(String(128), nullable=False, index=True, unique=True)
     password_hash: Mapped[Optional[str]] = mapped_column(String(256))
 
     authored     : WriteOnlyMapped['Activity'] = relationship(back_populates='author', foreign_keys='Activity.author_id')
@@ -17,17 +17,20 @@ class User(db.Model):
         return '<User {}>'.format(self.username)
 
 class Activity(db.Model):
-    _id          : Mapped[int]      = mapped_column(primary_key=True)
-    author_id    : Mapped[int]      = mapped_column(ForeignKey(User._id))
-    acceptor_id  : Mapped[int]      = mapped_column(ForeignKey(User._id))
-    kind         : Mapped[str]      = mapped_column(Enum('Request', 'Offer'), nullable=False)
-    status       : Mapped[str]      = mapped_column(Enum('Open', 'Closed', 'Pending', 'Completed'), nullable=False, default='Open')
-    description  : Mapped[str]      = mapped_column(String(256))
-    updated_at   : Mapped[datetime] = mapped_column(index=True, default=lambda: datetime.now(timezone.utc))
+    id           : Mapped[int]      = mapped_column(primary_key=True)
+    author_id    : Mapped[int]      = mapped_column(ForeignKey(User.id), nullable=False)
+    acceptor_id  : Mapped[int]      = mapped_column(ForeignKey(User.id), nullable=True)
+
+    type         : Mapped[str]      = mapped_column(Enum('Request', 'Offer'))
+    category     : Mapped[str]      = mapped_column(String(15), nullable=False)
+    description  : Mapped[str]      = mapped_column(String(100), nullable=False)
+
+    status       : Mapped[str]      = mapped_column(Enum('Open', 'Pending', 'Closed'), nullable=False, default='Open')
+    updated_at   : Mapped[datetime] = mapped_column(index=True, nullable=False, default=lambda: datetime.now(timezone.utc))
 
     author       : Mapped[User] = relationship(back_populates='authored', foreign_keys=[author_id])
     acceptor     : Mapped[User] = relationship(back_populates='accepted', foreign_keys=[acceptor_id])
 
 
     def __repr__(self):
-        return '<Request {}: "{}">'.format(self.title, self.description)
+        return '<Request {}: "{}">'.format(self.category, self.description)
