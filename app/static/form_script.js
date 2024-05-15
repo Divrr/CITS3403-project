3f5e6fa9bd5c7db46b4b5b8c3c43562797f27eb7
Zahra Vink <zahravink@gmail.com>
2024-05-15 12:16:22 +0800

    adjusted font colours for js colour changes and increased spacing for readablility

--- app/static/form_script.js
+++ app/static/form_script.js
@@ -1,45 +1,48 @@
-document.getElementById('offerRequestForm').addEventListener('submit', function(event) {
-    event.preventDefault();  // Prevent the default form submission
+document
+  .getElementById("offerRequestForm")
+  .addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent the default form submission
     var formData = new FormData(this);
-    fetch('/form', {  // The route that handles your form submission in Flask
-        method: 'POST',
-        body: formData,
+    fetch("/form", {
+      // The route that handles your form submission in Flask
+      method: "POST",
+      body: formData,
     })
-    .then(response => response.json())
-    .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         if (data.success) {
-            alert('Your activity has been created!');
-            window.location.href = document.body.getAttribute('data-index-url');  // Redirects to the index URL fetched from the data attribute in the body tag
+          alert("Your activity has been created!");
+          window.location.href = document.body.getAttribute("data-index-url"); // Redirects to the index URL fetched from the data attribute in the body tag
         } else {
-            alert('Failed to create activity. Please try again.');
+          alert("Failed to create activity. Please try again.");
         }
-    })
-    .catch(error => {
-        console.error('Error:', error);
-        alert('An error occurred. Please try again.');
-    });
-});
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        alert("An error occurred. Please try again.");
+      });
+  });
 
 function changeColor(selectObj) {
-    var idx = selectObj.selectedIndex;
-    var option = selectObj.options[idx];
-    var formContainer = document.querySelector(".form-container");
-    var formLabels = document.querySelectorAll(".mb-3 .inputdescription");
-    var submitButton = document.querySelector(".submit-btn");
+  var idx = selectObj.selectedIndex;
+  var option = selectObj.options[idx];
+  var formContainer = document.querySelector(".form-container");
+  var formLabels = document.querySelectorAll(".mb-3 .inputdescription");
+  var submitButton = document.querySelector(".submit-btn");
 
-    if (option.value === "Offer") {
-        formContainer.style.backgroundColor = "#a5d0ff";
-        formContainer.style.borderColor = "#2283ea";
-        formLabels.forEach(function (label) {
-            label.style.color = "#2283ea";
-        });
-        submitButton.style.borderColor = "#2283ea";
-    } else if (option.value === "Request") {
-        formContainer.style.backgroundColor = "#ffc484";
-        formContainer.style.borderColor = "#f48a18";
-        formLabels.forEach(function (label) {
-            label.style.color = "#f48a18";
-        });
-        submitButton.style.borderColor = "#f48a18";
-    }
+  if (option.value === "Offer") {
+    formContainer.style.backgroundColor = "#a5d0ff";
+    formContainer.style.borderColor = "#2283ea";
+    formLabels.forEach(function (label) {
+      label.style.color = "#074d97";
+    });
+    submitButton.style.borderColor = "#2283ea";
+  } else if (option.value === "Request") {
+    formContainer.style.backgroundColor = "#ffc484";
+    formContainer.style.borderColor = "#f48a18";
+    formLabels.forEach(function (label) {
+      label.style.color = "#bd5d00";
+    });
+    submitButton.style.borderColor = "#f48a18";
+  }
 }
