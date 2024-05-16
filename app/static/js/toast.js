bcf7e9290abee325dabd4e14095417bdb440efb1
Divrr <96827300+Divrr@users.noreply.github.com>
2024-05-16 09:05:23 +0800

    showToast works for accept_activity

---
+++ app/static/js/toast.js
@@ -0,0 +1,18 @@
+function showToast(message) {
+    var toastHTML = 
+    `<div class="toast" role="alert" aria-live="assertive" aria-atomic="true" data-bs-delay="2000">
+        <div class="toast-header">
+            <strong class="me-auto">Notification</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="toast" aria-label="Close"></button>
+        </div>
+        <div class="toast-body">
+            ${message}
+        </div>
+    </div>`;
+
+    var toastContainer = $('.toast-container');
+    toastContainer.append(toastHTML);
+    var toastEl = toastContainer.find('.toast').last();
+    var toast = new bootstrap.Toast(toastEl);
+    toast.show();
+}
