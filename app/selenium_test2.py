948449ba55e107f4cf35c5373a525fe7fdef462b
hhK2001-blast <aviv2silman@gmail.com>
2024-05-17 17:54:16 +0800

    added further addition of testing, selenium in function format, unittests added, blueprints were needed

---
+++ app/selenium_test2.py
@@ -0,0 +1,72 @@
+import multiprocessing
+import time
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from unittest import TestCase
+
+from app import create_app, db
+from config import TestConfig
+
+class TestGroupCreation(TestCase):
+    def setUp(self):
+        self.app = create_app(TestConfig)
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        db.create_all()
+        self.driver = webdriver.Chrome()
+        self.driver.get('http://127.0.0.1:5000')
+    
+    def tearDown(self):
+        self.driver.quit()
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()
+
+    def test_login(self):
+        self.driver.get('http://127.0.0.1:5000/login')
+        time.sleep(3)
+        username_field = self.driver.find_element("xpath", "//input[@name='username']")
+        password_field = self.driver.find_element("xpath", "//input[@name='password']")
+        username_field.send_keys("user1")
+        password_field.send_keys("password")
+        self.driver.execute_script('window.scrollBy(0,240)')
+        time.sleep(3)
+        submit_button = self.driver.find_element("xpath", "//input[@name='submit']")
+        submit_button.click()
+
+    def test_signup(self):
+        self.driver.get('http://127.0.0.1:5000/signup')
+        username_field = self.driver.find_element("xpath", "//input[@name='username']")
+        email_field = self.driver.find_element("xpath", "//input[@name='email']")
+        password_field = self.driver.find_element("xpath", "//input[@name='password']")
+        password2_field = self.driver.find_element("xpath", "//input[@name='password2']")
+        time.sleep(8)
+        self.driver.execute_script('window.scrollBy(0,280)')
+        time.sleep(3)
+        submit_button = self.driver.find_element("xpath", "//input[@name='submit']")
+
+        username_field.send_keys("testuser")
+        email_field.send_keys("aviv2silman@gmail.com")
+        password_field.send_keys("password64")
+        self.driver.execute_script('window.scrollBy(0,190)')
+        password2_field.send_keys("password64")
+        self.driver.execute_script('window.scrollBy(0,190)')
+        submit_button.click()
+
+    def test_create_accountlink(self):
+        self.driver.get('http://127.0.0.1:5000/login')
+        self.driver.execute_script('window.scrollBy(0,240)')
+        link_to_signup = self.driver.find_element("xpath", "//a[@href='/signup']")
+        self.driver.execute_script('window.scrollBy(0,240)')
+        time.sleep(4)
+        link_to_signup.click()
+        
+    #def test_alreadyhaveaccountlink(self):
+    #    self.driver.get('http://127.0.0.1:5000/signup')
+    #    self.driver.execute_script('window.scrollBy(0,240)')
+    #    time.sleep(2)
+    #    link_to_login = self.driver.find_element("xpath", "//a[@href='/login']")
+    #    self.driver.execute_script('window.scrollBy(0,240)')
+    #    time.sleep(2)
+        #link_to_login.click()
